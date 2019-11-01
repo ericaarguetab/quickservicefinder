922a23aa8a04ffabc57f49ca14e035ffb672a389
Erica Argueta <ericaarguetab@gmail.com>
2019-11-01 11:03:42 -0600

    updating txt to sql file

---
+++ sector_subsector_insert.sql
@@ -0,0 +1,37 @@
+INSERT INTO `quickservicefinder`.`sector` (`idsector`, `name`) VALUES ('1', 'Servicios Profesionales');
+INSERT INTO `quickservicefinder`.`sector` (`idsector`, `name`) VALUES ('2', 'Publicidad');
+INSERT INTO `quickservicefinder`.`sector` (`idsector`, `name`) VALUES ('3', 'Salud y Sanidad');
+INSERT INTO `quickservicefinder`.`sector` (`idsector`, `name`) VALUES ('4', 'Turismo');
+INSERT INTO `quickservicefinder`.`sector` (`idsector`, `name`) VALUES ('5', 'Ocio');
+INSERT INTO `quickservicefinder`.`sector` (`idsector`, `name`) VALUES ('6', 'Cultura');
+INSERT INTO `quickservicefinder`.`sector` (`idsector`, `name`) VALUES ('7', 'Administración');
+INSERT INTO `quickservicefinder`.`sector` (`idsector`, `name`) VALUES ('8', 'Tecnología');
+INSERT INTO `quickservicefinder`.`sector` (`idsector`, `name`) VALUES ('9', 'Educación');
+INSERT INTO `quickservicefinder`.`sector` (`idsector`, `name`) VALUES ('10', 'Otros');
+INSERT INTO `quickservicefinder`.`sector` (`idsector`, `name`) VALUES ('11', 'Construcción y Mantenimiento');
+INSERT INTO `quickservicefinder`.`sector` (`idsector`, `name`) VALUES ('12', 'Alimentos');
+
+
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('1', '7', 'Renta de Vehículos');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('2', '7', 'Servicio de Transporte Privado');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('3', '7', 'Transporte de Materia Prima');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('4', '9', 'Primaria');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('5', '9', 'Secundaria');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('6', '9', 'Universitarios');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('7', '9', 'Idiomas');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('8', '9', 'Música');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('9', '9', 'Baile');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('10', '9', 'Acondicionamiento Físico, Gimnasia');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('11', '8', 'Reparación y Mantenimiento de Equipos');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('12', '8', 'Desarrollo de sistemas y aplicaciones');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('13', '8', 'Consultorías');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('14', '8', 'Equipos Computarizados y Electrónicos');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('15', '3', 'Personal de Limpieza');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('16', '3', 'Exterminadores');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('17', '3', 'Clínicas');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('18', '11', 'Carpintería');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('19', '11', 'Fontanería');
+INSERT INTO `quickservicefinder`.`subsector` (`idsubsector`, `idsector`, `names`) VALUES ('20', '11', 'Mantenimiento para el hogar/Construcción');
+
+
+
